0d5f91322425e1bf5941144b10e849f1540ed1f9
xxz <xxz911@rambler.ru>
2023-04-22 17:01:08 +0300

    Создаем очередь(queue)

---
+++ 15.Making_queue_(queue).py
@@ -0,0 +1,18 @@
+import collections
+
+q = collections.deque()
+print('Пустая очередь', q)
+
+# Добавляем элементы в очеред(в конец очереди)
+q.append(1)
+q.append(2)
+q.append(3)
+print(q)
+
+# Удалит первый элемент и вернет его значение
+q.popleft()
+print(q)
+
+# Можем реализовать и Стек используя pop() и удалим последний элемент
+q.pop()
+print(q)
